876609e71330eec5ece100b0394bda1119c927df
JonathanSolvesProblems <46333281+JonathanSolvesProblems@users.noreply.github.com>
2020-02-22 08:13:48 -0500

    making sure it's most recent

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import json
+from difflib import get_close_matches # library to compare text
 
 data = json.load(open("data.json"))
 
 def definition(token):
-    return data[token]
+    token = token.lower() # allows for capital and lower case letters of words to be accepted
+    if token in data:
+        return data[token]
+    elif len(get_close_matches(token, data.keys())) > 0:
+        return "Did you mean %s instead? " % get_close_matches(token, data.keys())[0]
+    else: # if word is not in dictionary
+        return "The word does not exist in the dictionary. Please verify your word and try again."
 
 token = input("Enter word: ")
 
